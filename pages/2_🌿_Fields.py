9f1832f2d086a5f0e0e28587ba0ccef938c7e7bd
Oliver Lopez <lopezv.oliver@gmail.com>
2024-05-02 15:12:01 +0300

    Color based on style

--- pages/2_🌿_Fields.py
+++ pages/2_🌿_Fields.py
@@ -12,6 +12,15 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 gdata = conn.read(spreadsheet=sheet1_url).dropna()  # Different sheet than the weather one.
 gdata['geometry'] = gdata.geometry.apply(wkt.loads)
 gdf = gpd.GeoDataFrame(gdata)
+wheat_color =  "#FFC107"
+potato_color = "#E66100"
+watermelon_color = "#086608"
+
+color_dict = {
+    "Wheat": wheat_color,
+    "Potato": potato_color,
+    "Watermelon": watermelon_color
+}
 
 
 markdown = """
@@ -27,6 +36,15 @@ st.title("2023 INMA fields map")
 m = leafmap.Map(
     locate_control=True, latlon_control=True, draw_export=False, minimap_control=True
 )
-m.add_gdf(gdf, layer_name="Fields")
+def style(feature):
+    return {
+        "fillColor": color_dict.get(feature["properties"]["Crop"],"#000000"),
+        "color": "black",
+        "fillOpacity": 1
+    }
+
+m.add_gdf(gdf, layer_name="Fields",
+          style_function = style,
+)
 m.to_streamlit(height=700)
 
