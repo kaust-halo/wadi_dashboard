a34fbfb6f19cb55edd97135b6c7c348f67dbed8c
Oliver Lopez <lopezv.oliver@gmail.com>
2024-05-02 10:15:56 +0300

    Revert changes after leafmap tests

--- 1_⛅_Weather.py
+++ 1_⛅_Weather.py
@@ -7,7 +7,6 @@ import pandas as pd
 import plotly.graph_objects as go
 import warnings
 import datetime 
-import leafmap.foliumap as leafmap
 
 warnings.filterwarnings('ignore')
 
@@ -162,10 +161,6 @@ plot_et.update_layout(xaxis=dict(range=pdate_range, uirevision=True), uirevision
 # Main panel 
 st.markdown("## Wadi ad-Dawasir weather data")
 
-# TEMPORARY TESTING LEAFMAP
-m = leafmap.Map(minimap_control=True)
-m.add_basemap("OpenTopoMap")
-m.to_streamlit(height=500)
 
 
 st.plotly_chart(plot_temperature, use_container_width=True)
--- pages/2_🌿_Fields.py
+++ pages/2_🌿_Fields.py
@@ -3,15 +3,15 @@ Wadi ad-Dawasir INMA fields (2023 only for now)
 """
 import streamlit as st
 import leafmap.foliumap as leafmap
-#from streamlit_gsheets import GSheetsConnection
-#import geopandas as gpd
-#from shapely import wkt
+from streamlit_gsheets import GSheetsConnection
+import geopandas as gpd
+from shapely import wkt
 
-#sheet1_url = st.secrets["gsheets"]["geo"]
-#conn = st.connection("gsheets", type=GSheetsConnection)
-#gdata = conn.read(spreadsheet=sheet1_url).dropna()  # Different sheet than the weather one.
-#gdata['geometry'] = gdata.geometry.apply(wkt.loads)
-#gdf = gpd.GeoDataFrame(gdata)
+sheet1_url = st.secrets["gsheets"]["geo"]
+conn = st.connection("gsheets", type=GSheetsConnection)
+gdata = conn.read(spreadsheet=sheet1_url).dropna()  # Different sheet than the weather one.
+gdata['geometry'] = gdata.geometry.apply(wkt.loads)
+gdf = gpd.GeoDataFrame(gdata)
 
 
 markdown = """
@@ -27,6 +27,6 @@ st.title("2023 INMA fields map")
 m = leafmap.Map(
     locate_control=True, latlon_control=True, draw_export=False, minimap_control=True
 )
-#m.add_gdf(gdf, layer_name="Fields")
+m.add_gdf(gdf, layer_name="Fields")
 m.to_streamlit(height=700)
 
